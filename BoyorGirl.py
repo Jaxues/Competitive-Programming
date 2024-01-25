31ec4073a6c295f46d2e923abed806789c81b802
Avery <jholmesoliver@gmail.com>
2024-01-25 15:30:47 +1300

    Remove Duplicate files

--- BoyorGirl.py
+++
@@ -1,5 +0,0 @@
-size=list(map(set,list(input().split())))
-if len(size[0])%2==0:
-    print("CHAT WITH HER!")
-else:
-    print("IGNORE HIM!")
--- watermelon.py
+++
@@ -1,5 +0,0 @@
-wm=int(input())
-if wm%2 ==0 and wm>3:
-    print("YES")
-else:
-    print("NO")
