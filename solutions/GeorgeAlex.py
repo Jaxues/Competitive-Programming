b4086688179cca219b9a3167f7e102d130de6db7
Avery <jholmesoliver@gmail.com>
2024-02-10 15:28:29 +1300

    Solved 3 problems tried to solve one other

---
+++ solutions/GeorgeAlex.py
@@ -0,0 +1,11 @@
+num_rooms=int(input())
+avaliable=[]
+num_avaliable_rooms=0
+for room in range(num_rooms):
+    avaliable.append(list(map(int, input().split())))
+
+
+for avaliable_room in avaliable:
+    if avaliable_room[0]!=avaliable_room[1] and avaliable_room[0]+2 < avaliable_room[1]:
+        num_avaliable_rooms+=1      
+print(num_avaliable_rooms)
