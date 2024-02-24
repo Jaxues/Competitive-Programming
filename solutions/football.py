b5b35ed877b73ad26be99f788aac9a463c2b84da
Avery <jholmesoliver@gmail.com>
2024-02-24 17:54:20 +1300

    Solved codeforces problem Candies and Petya string, improved solution on football.

--- solutions/football.py
+++ solutions/football.py
@@ -1,18 +1,22 @@
 lineup=input()
-counter=0
-unlucky_list=[]
+unluck_counter=0
+unluck_max=0
+previous=0
 for player in range(len(lineup)):
-    if player+1 < len(lineup):
-        print(lineup[player],lineup[player+1])
+    if player+1 < len(lineup) :
+
         if lineup[player]==lineup[player+1]:
-            counter+=1
-            print(counter)
+            unluck_counter+=1
+        elif lineup[player]==lineup[player-1] and player!=0:
+            unluck_counter+=1 
         else:
-            unlucky_list.append(counter)
-            conter=0
-print(unlucky_list)
-if max(unlucky_list)>=7:
+            unluck_counter=0
+        if unluck_counter>unluck_max:
+            unluck_max=unluck_counter
+        print(f"first player is {lineup[player]} second player is {lineup[player+1]} They are {lineup[player]==lineup[player+1]}, unluck_counter is {unluck_counter}, unluck_max is {unluck_max}")
+        print(f"Previous was {lineup[player-1]}. They are {lineup[player-1]==lineup[player]}")
+print(unluck_max)
+if unluck_max >= 7:
     print("YES")
 else:
     print("NO")
-
