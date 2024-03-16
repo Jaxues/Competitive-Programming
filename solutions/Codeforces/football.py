1080d39727ff651972d77fa71a448c1a18180088
Avery <jholmesoliver@gmail.com>
2024-03-16 16:52:39 +1300

    Updated football.py

--- solutions/Codeforces/football.py
+++ solutions/Codeforces/football.py
@@ -13,9 +13,6 @@ for player in range(len(lineup)):
             unluck_counter=0
         if unluck_counter>unluck_max:
             unluck_max=unluck_counter
-        print(f"first player is {lineup[player]} second player is {lineup[player+1]} They are {lineup[player]==lineup[player+1]}, unluck_counter is {unluck_counter}, unluck_max is {unluck_max}")
-        print(f"Previous was {lineup[player-1]}. They are {lineup[player-1]==lineup[player]}")
-print(unluck_max)
 if unluck_max >= 7:
     print("YES")
 else:
